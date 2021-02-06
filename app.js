c16ee4deadd295fc0177bd20096d681bfd010aad
Abanoub <mrbob539@gmail.com>
2021-02-06 17:53:26 +0200

    emit message to server and broadcasts it to clients

--- app.js
+++ app.js
@@ -16,27 +16,37 @@ const server = app.listen(PORT, () => {
 // contains all the connected sockets
 let connectedSockets = new Set()
 
-// initialize socket.io using the server we created
+// initialize socket.io using the server
 const io = require('socket.io')(server)
 
 io.on('connection', (socket) => {
+    /** when a new socket is connected */
     // any client will connect to the socket.io will be logged here
     console.log(colors.yellow(`A socket is connected: ${socket.id}`))
     // add each client connected socket to the connected sockets set
     connectedSockets.add(socket.id)
 
+    /** when the total clients is increased */
     // the change in the size when we add a new socket will trigger this event
     // we will emit this event to the client to log the size of the sockets there
     io.emit('clients-total', connectedSockets.size)
 
-    // when a socket is disconnected
     socket.on('disconnect', () => {
+        /** when a socket is disconnected */
         console.log(colors.red(`A socket is disconnected ${socket.id}`))
         // remove the disconnected socket from the connected sockets
         connectedSockets.delete(socket.id)
 
+        /** when the total clients is decreased */
         // the change in the size when we delete a disconnected socket will trigger this event
         // we will emit this event to the client to log the size of the sockets there
         io.emit('clients-total', connectedSockets.size)
+    })
+
+    /** the new message that will be emitted from the client */
+    socket.on('message', (data) => {
+        console.log(data)
+        // now we wanna emit it to all clients except the one who sent us this message
+        socket.broadcast.emit('chat-message', data)
     })
 })
--- public/main.js
+++ public/main.js
@@ -4,8 +4,38 @@ const socket = io()
 // this will hold how many sockets connected to the server in the real time
 const clientsTotal = document.getElementById('clients-total')
 
+const messageContainer = document.getElementById('message-container')
+const nameInput = document.getElementById('name-input')
+const messageForm = document.getElementById('message-form')
+const messageInput = document.getElementById('message-input')
+
+// send a new message from the submit button of this form
+messageForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    sendMessage()
+})
+
 // this event wil get fired in the server and send us this data to log in the browser
 // this event will be emitted from the server to tell the client here 
 socket.on('clients-total', (data) => {
     clientsTotal.innerText = `Total Clients: ${data}`
+})
+
+const sendMessage = () => {
+    // create the message we wanna send to the server
+    const data = {
+        // the sender of the message
+        name: nameInput.value, 
+        // the body of the message
+        message: messageInput.value,
+        // the timestamp of the message
+        dateTime: new Date()
+    }
+    // now emit the message to the server
+    socket.emit('message', data)
+}
+
+// listen to the message that the server is broadcasting to all the connected clients
+socket.on('chat-message', (data) => {
+    console.log(data)
 })
